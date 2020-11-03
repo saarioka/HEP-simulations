60ec8d004a85da07921146d45d94c5e0864a4cc7
Santeri Saariokari <santeri.saariokari@gmail.com>
2020-11-03 14:30:51 +0200

    starting plots for dynamically generated files

--- generate_runs.py
+++ generate_runs.py
@@ -1,38 +1,28 @@
 import os
 import subprocess
 
-def um_to_cm(a):
-    return a/10000
+cycles = 2
+filenumber = '%03d' % cycles
 
-def kev_to_gev(a):
-    return a/1e6
+input_files = [f for f in os.listdir('.') if os.path.isfile(f) and (('si-' in f or 'cdte-' in f) and '.inp' in f and 'echo' not in f)]
+input_files = sorted(input_files)
+print(len(input_files), ' input files found')
 
-infile = open('template.inp')
-lines = infile.readlines()
+units = [21, 22, 94, 96, 97]
+filetypes = ['.bnx', '.bnx', '.trk', '.bnn', '.bnn']
+programs = ['usxsuw', 'usxsuw', 'ustsuw', 'usbsuw', 'usbsuw']
 
-#Si: thickness 320um - 900um, energies 30keV - 1331keV
-for t in range(320, 901, 58):
-    for e in range(30, 1331, 50):
-        lines[5] = '#define BULK_THICKNESS ' + str(um_to_cm(t)) + '\n'
-        lines[6] = '#define BEAM_ENERGY ' + str(kev_to_gev(e)) + '\n'
-        lines[7] = '#define BULK_MATERIAL_SILICON\n'
-        lines[8] = '!#define BULK_MATERIAL_CDTE\n'
-        fn = 'dyn_si_' + str(t) + '_' + str(e) + '.inp'
-        print(fn, t, e)
-        outfile = open(fn, 'w+')
-        outfile.writelines(lines)
-        subprocess.call('/usr/bin/nohup /media/santeri/linux-storage/fluka/bin/rfluka -M 1 ' + fn, shell=True)
+for fn in input_files:
+    subprocess.run(['nohup', '/media/santeri/linux-storage/fluka/bin/rfluka', '-M', str(cycles), fn])
 
+    for u in range(len(units)):
+        binaries = [f for f in os.listdir('.') if os.path.isfile(f) and (('si-' in f or 'cdte-' in f) and '_fort.' + str(units[u]) in f)]
+        binaries = sorted(binaries)
+        print(binaries)
 
-#CdTe: thickness 1mm - 2mm, energies 30keV - 1331keV
-for t in range(1000, 2000, 58):
-    for e in range(30, 1331, 50):
-        lines[5] = '#define BULK_THICKNESS ' + str(um_to_cm(t)) + '\n'
-        lines[6] = '#define BEAM_ENERGY ' + str(kev_to_gev(e)) + '\n'
-        lines[7] = '!#define BULK_MATERIAL_SILICON\n'
-        lines[8] = '#define BULK_MATERIAL_CDTE\n'
-        fn = 'dyn_cdte_' + str(t) + '_' + str(e) + '.inp'
-        print(fn, t, e)
-        outfile = open(fn, 'w+')
-        outfile.writelines(lines)
-        subprocess.call('/usr/bin/nohup /media/santeri/linux-storage/fluka/bin/rfluka -M 1 ' + fn, shell=True)
+        command = '\n'.join(binaries) + '\n' + '\n' + fn.split('.')[0] + '_' + str(units[u]) + filetypes[u] + '\n'
+        subprocess.run([programs[u]], input=bytes(command, 'utf-8'))
+
+    binaries = [f for f in os.listdir('.') if os.path.isfile(f) and '_fort.' in f]
+    for b in binaries:
+        os.remove(b)
